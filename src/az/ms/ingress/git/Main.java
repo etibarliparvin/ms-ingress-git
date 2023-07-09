8d84495d7de5b6d728d431db557dd2fd9df0bc1d
Parvin Etibarli <etibarliparvin@gmail.com>
2023-07-09 18:19:06 +0400

    BSA-990: Remove suffix

--- src/az/ms/ingress/git/Main.java
+++ src/az/ms/ingress/git/Main.java
@@ -2,7 +2,7 @@ package az.ms.ingress.git;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
+        System.out.println("Hello World!");
+        System.out.println("Hello Ingre");
     }
 }
